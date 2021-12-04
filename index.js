7ee86cae94be6a39872fc9c268c1b49af839e27a
Isaac-the-Man <steven97102@gmail.com>
2021-12-04 13:23:04 -0500

    roast command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client, Intents } = require('discord.js')
 const fs = require('fs')
 const fsPromises = fs.promises
 const moment = require('moment')
+const axios = require('axios')
 
 
 // CONSTANTS (from ENV)
@@ -52,12 +53,22 @@ function readCartesiProfitStats() {
 	return profits
 }
 
+// get roast
+async function getRandomRoast() {
+	try {
+		const res = await axios.get('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+		return res.data.insult
+	} catch(e) {
+		console.log(e)
+	}
+}
+
 // Bot hooks
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
-});
+})
 
 client.on('messageCreate', async (msg) => {
 	// prevent bot
@@ -65,16 +76,32 @@ client.on('messageCreate', async (msg) => {
 		return false
 	}
 	// message hooks
-	if (msg.content === '!pool now') {
+	if (msg.content === '!pool now') {	// current balance in pool
 		await readCartesiPoolCSV()
 		const last = cartesiData.at(-1)
 		const now = moment(last.timestamp)
-		msg.reply(`As of ${now.format('llll')}, you have ${last.balance} ctsi in the pool.`)
-	} else if (msg.content === '!pool stats') {
+		msg.reply(`As of ${now.format('llll')}, you have ${last.balance} CTSI in the pool.`)
+	} else if (msg.content === '!pool stats') {	// basic stats for CTSI pool
 		await readCartesiPoolCSV()
 		const profits = readCartesiProfitStats()
 		msg.reply(`You've earned ${profits[0]} in the past 24hrs, ${profits[1]} in the past 72hrs, and ${profits[2]} in the past week.`)
-	}	
+	} else if (msg.content.slice(0,6).trim() === '!roast') {	// roast someone
+		const targets = msg.mentions.members.toJSON()
+		if (targets.length <= 0) {
+			msg.reply("You didn't specify a target (you stupid).")
+		} else {
+			if (msg.mentions.has(msg.guild.members.cache.get('537305396348583948'))) {
+				msg.reply(':pinched_fingers:')
+				return
+			}
+			const roast = await getRandomRoast()
+			if (roast) {
+				msg.channel.send(`${msg.mentions.members.toJSON()} ${roast}`)
+			} else {
+				msg.reply("Ah shit something's wrong...")
+			}
+		}
+	}
 })
 
 client.login(process.env.CLIENT_TOKEN)
